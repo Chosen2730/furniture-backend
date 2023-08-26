546d2be82a579d09fd476e39c9fe3492791dc290
Robinson Simon <cf.youvarsity@gmail.com>
2023-08-26 10:57:16 +0100

    removed port form the .env

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ mongoose
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: "true" }));
+app.use("/api/products", productRouter);
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Furniture backend");
 });
-app.use("/api/products", productRouter);
-app.use(cors());
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );
